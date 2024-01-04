2b1deb5d3857892c1087b081c4f4077bcb15847c
ODJOUOYE Adiyo Fouad <109107244+AdiyoFouad@users.noreply.github.com>
2024-01-04 18:38:27 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+'''
+A module for working with Pascal's triangle.
+'''
 
+def calculate_binomial_coefficient(n, k):
+    '''Calculates the binomial coefficient "n choose k".
+    '''
+    if k == 0 or k == n:
+        return 1
+    return calculate_binomial_coefficient(n - 1, k - 1) + calculate_binomial_coefficient(n - 1, k)
+
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = [calculate_binomial_coefficient(i, j) for j in range(i + 1)]
+        triangle.append(line)
+    return triangle
